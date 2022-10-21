8ca6bb9fe47c3bd986a49932ff2e371297245aed
Rônald Macário <ronaldmacario13@gmail.com>
2022-10-20 21:08:49 -0300

    Atualização 201022

--- Lógica de Programação OO/Módulo 2 - Introdução a Programação/HipertextoPraticando.py
+++ Lógica de Programação OO/Módulo 2 - Introdução a Programação/HipertextoPraticando.py
@@ -1,16 +1,39 @@
-import time
+# import time
+
+# contador = 0
+# array = 44 *["_"]
+
+# print("Iniciando contagem!")
+
+# for i in range(1,46):
+#     result = [''.join(array)]
+#     print(f"{result} {i}/45 sec")
+#     if contador < 44:
+#         array[contador] = "|"
+#     contador += 1
+#     time.sleep(1)
+
+# print("Acabou o tempo de descanço!")
+
+import getpass 
+
+#--------Jogo de adivinha-----------#
+
+palavra_secreta = getpass.getpass('Digite a palavra secreta: ')
+adivinhar_palavra = input("Digite a palavra: ")
 
 contador = 0
-array = 44 *["_"]
 
-print("Iniciando contagem!")
-
-for i in range(1,46):
-    result = [''.join(array)]
-    print(f"{result} {i}/45 sec")
-    if contador < 44:
-        array[contador] = "|"
+for i in palavra_secreta:
     contador += 1
-    time.sleep(1)
 
-print("Acabou o tempo de descanço!")
+while palavra_secreta != adivinhar_palavra and contador > 1:
+    if palavra_secreta == adivinhar_palavra:
+        print("Parabens!")
+    else:
+         contador = contador - 1
+         adivinhar_palavra = input('Voce so tem {0} tentativas, tente novamente: '.format(contador))
+if contador == 1:
+    print("Voce exedeu a quantidade de tentativas!")
+else:
+    print("Parabens!")
